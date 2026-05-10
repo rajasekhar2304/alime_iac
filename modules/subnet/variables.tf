1488416ac18e042eed40823b77b74aedfb41e5c0
root <root@terraform-host.a1pnvdim1tpe1c2feuej0d4yid.rx.internal.cloudapp.net>
2026-05-10 16:06:10 +0000

    fmt applied

--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -1,12 +1,12 @@
 variable "subnets" {
   description = "Map of subnets to create"
   type = map(object({
-    name                 = string
-    resource_group_name  = string
-    virtual_network_name = string
-    address_prefixes     = list(string)
-    service_endpoints = optional(list(string), [])
-    private_endpoint_network_policies = optional(string, "Enabled")
-    private_link_service_network_policies_enabled = optional(bool, true) 
+    name                                          = string
+    resource_group_name                           = string
+    virtual_network_name                          = string
+    address_prefixes                              = list(string)
+    service_endpoints                             = optional(list(string), [])
+    private_endpoint_network_policies             = optional(string, "Enabled")
+    private_link_service_network_policies_enabled = optional(bool, true)
   }))
 }
