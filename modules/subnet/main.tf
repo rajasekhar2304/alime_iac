bb93faafc8e0fc447f98d21b27e537e3aa523921
Raja Sekhar Gurugubelli <rgurugubelli@cog.int>
2026-05-10 21:35:44 +0530

    subnet module created

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -4,4 +4,7 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = each.value.resource_group_name
   virtual_network_name = each.value.virtual_network_name
   address_prefixes     = each.value.address_prefixes
+  service_endpoints = each.value.service_endpoints
+  private_endpoint_network_policies = each.value.private_endpoint_network_policies
+  private_link_service_network_policies_enabled = each.value.private_link_service_network_policies_enabled
 }
--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -5,5 +5,8 @@ variable "subnets" {
     resource_group_name  = string
     virtual_network_name = string
     address_prefixes     = list(string)
+    service_endpoints = optional(list(string), [])
+    private_endpoint_network_policies = optional(string, "Enabled")
+    private_link_service_network_policies_enabled = optional(bool, true) 
   }))
 }
